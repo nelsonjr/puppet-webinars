f35e4c848096bbfa57c754ae4ebfd8343c98bb59
Cody Herriges <cody@herriges.org>
2017-09-11 20:37:56 -0700

    Updates profile with some documentation
      This commit puts in place a number of comments to help inform those
      that might look at it for reference in the future.

--- control/profile/facts.d/wp_user_ssh_pub.sh
+++ control/profile/facts.d/wp_user_ssh_pub.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+# Super simple external fact written in bash.  If it detects the presence
+# of a SSH public key for the "wp-user", it makes it available to Puppet
+# as the "wp_user_ssh_pub" fact.
+#
+# https://docs.puppet.com/facter/3.8/custom_facts.html#external-facts
+
 if [ -e '/home/wp-user/.ssh/id_rsa.pub' ];then
   echo "wp_user_ssh_pub=$(cat /home/wp-user/.ssh/id_rsa.pub)"
 fi
--- control/profile/manifests/wordpress/migrate/prep.pp
+++ control/profile/manifests/wordpress/migrate/prep.pp
@@ -1,9 +1,23 @@
+# Class: profile::wordpress::migrate::prep
+#
+# Queries and places into the wp-user's the SSH public keys published by source
+#
+# Actions:
+#   - Looks up the "wp_user_ssh_pub" value
+#   - Puts discovered key(s) into the correct place
+#
 class profile::wordpress::migrate::prep {
 
+  # Our source machine needs to log into our target as the "wp-user" so it
+  # can rsync images.
+  #
+  # Query will return all the "wp_user_ssh_pub" facts.
   $source_keys_query = 'facts { name = "wp_user_ssh_pub" }'
 
+  # Run query previously defined against PuppetDB and return the value found
   $source_keys = any2array(puppetdb_query($source_keys_query)[0]['value'])
 
+  # Place the discovered value into authorized_keys file.
   file { '/home/wp-user/.ssh/authroized_keys':
     ensure   => file,
     content  => $source_keys.sort.join("\n"),
