28c975324b8b2be33300022c00e915a284e6c68b
Nelson Araujo <nelsona@google.com>
2017-09-12 18:01:02 +0000

    Reverting to single, now smarter, script.

---
+++ migrate-wordpress/wp-create.pp
@@ -0,0 +1,177 @@
+gauth_credential { 'cred':
+  provider => serviceaccount,
+  path     => '/opt/admin/my_account.json',
+  scopes   => [
+    'https://www.googleapis.com/auth/cloud-platform',
+    'https://www.googleapis.com/auth/sqlservice.admin',
+  ],
+}
+
+$legacy_server = '13.59.50.69'
+
+gcompute_region { 'us-central1':
+  project    => 'graphite-demo-puppet-webinar1',
+  credential => 'cred',
+}
+
+gcompute_zone { 'us-central1-c':
+  project    => 'graphite-demo-puppet-webinar1',
+  credential => 'cred',
+}
+
+gcompute_machine_type { 'n1-standard-2':
+  zone       => 'us-central1-c',
+  project    => 'graphite-demo-puppet-webinar1',
+  credential => 'cred',
+}
+
+gcompute_network { 'default':
+  ensure     => present,
+  project    => 'graphite-demo-puppet-webinar1',
+  credential => 'cred',
+}
+
+if $machine_name == undef {
+  fail('Fact "machine_name" has to be defined')
+}
+
+gcompute_address { $machine_name:
+  ensure     => present,
+  region     => 'us-central1',
+  project    => 'graphite-demo-puppet-webinar1',
+  credential => 'cred',
+}
+
+# Fetch the IP address of the VM
+$fn_auth = gauth_credential_for_function(
+  'serviceaccount', '/opt/admin/my_account.json',
+  ['https://www.googleapis.com/auth/cloud-platform']
+)
+
+$wp_address = gcompute_address_ip($machine_name, 'us-central1',
+                                  'graphite-demo-puppet-webinar1', $fn_auth)
+
+if ! $wp_address {
+  warning("Skipping SQL setup because IP address is just allocated. Run again.")
+} else {
+  info("Wordpress server @ ${wp_address}")
+
+  gsql_instance { $machine_name:
+    ensure           => present,
+    database_version => 'MYSQL_5_7',
+    settings         => {
+      ip_configuration => {
+        authorized_networks => [
+          {
+            name  => 'Admin workstation',
+            value => '104.197.210.80/32',
+          },
+          {
+            name  => 'Current server',
+            value => "${wp_address}/32",
+          },
+          {
+            name  => 'Legacy server',
+            value => "${legacy_server}/32",
+          },
+        ],
+      },
+      tier             => 'db-n1-standard-1',
+    },
+    region           => 'us-central1',
+    project          => 'graphite-demo-puppet-webinar1',
+    credential       => 'cred',
+  }
+
+  gsql_user { 'wordpress':
+    ensure     => present,
+    password   => 'secret-password',
+    host       => '%',
+    instance   => $machine_name,
+    project    => 'graphite-demo-puppet-webinar1',
+    credential => 'cred',
+  }
+}
+
+gcompute_disk { $machine_name:
+  ensure       => present,
+  size_gb      => 50,
+  source_image => gcompute_image_family('centos-7', 'centos-cloud'),
+  zone         => 'us-central1-c',
+  project      => 'graphite-demo-puppet-webinar1',
+  credential   => 'cred',
+}
+
+# Fetch the IP address of the SQL database
+$wp_db_address = gsql_instance_ip($machine_name,
+                                  'graphite-demo-puppet-webinar1', $fn_auth)
+
+if ! $wp_db_address {
+  warning("Skipping server because SQL instance is just allocated. Run again.")
+} else {
+  info("Wordpress database @ ${wp_db_address}")
+
+  gcompute_instance { $machine_name:
+    ensure             => present,
+    machine_type       => 'n1-standard-2',
+    disks              => [
+      {
+        boot        => true,
+        source      => $machine_name,
+        auto_delete => true,
+      },
+    ],
+    metadata           => [
+      {
+        'startup-script-url'  => 
+          'https://puppet-enterprise:8140/packages/current/install.bash',
+        'database-ip-address' => $wp_db_address,
+      },
+    ],
+    network_interfaces => [
+      {
+        network        => 'default',
+        access_configs => [
+          {
+            name   => 'External NAT',
+            nat_ip => $machine_name,
+            type   => 'ONE_TO_ONE_NAT',
+          },
+        ],
+      },
+    ],
+    zone               => 'us-central1-c',
+    project            => 'graphite-demo-puppet-webinar1',
+    credential         => 'cred',
+  }
+}
+
+gdns_managed_zone { 'eclipsecorner-org':
+  ensure     => present,
+  dns_name   => 'eclipsecorner.org.',
+  project    => 'graphite-demo-puppet-webinar1',
+  credential => 'cred',
+}
+
+if ! $wp_address {
+  warning("Skipping DNS setup because IP address is just allocated. Run again.")
+} else {
+  if $facts['staging'] {
+    warning('Setting up the staging DNS record: staging.eclipsecorner.org.')
+    $dns_rr_name = 'staging'
+  } else {
+    $dns_rr_name = 'www'
+  }
+
+  info("${dns_rr_name}.eclipsecorner.org @ ${wp_address}")
+
+  gdns_resource_record_set { "${dns_rr_name}.eclipsecorner.org.":
+    ensure       => present,
+    managed_zone => 'eclipsecorner-org',
+    type         => 'A',
+    target       => $wp_address,
+    ttl          => 5,
+    project      => 'graphite-demo-puppet-webinar1',
+    credential   => 'cred',
+  }
+}
