f210e7967feac313419c61949cb3d87ab9204a80
Cody Herriges <cody@herriges.org>
2017-09-11 20:37:56 -0700

    Updates profile with some documentation
      This commit puts in place a number of comments to help inform those
      that might look at it for reference in the future.

--- control/profile/manifests/wordpress.pp
+++ control/profile/manifests/wordpress.pp
@@ -1,28 +1,50 @@
+# Class: profile::wordpress
+#
+# This class contains all site specific configuration for installing Wordpress
+# for our demo
+#
+# Actions:
+#   - Setup Wordpress database
+#   - Install Wordpress
+#   - Enable Apache vhost for Wordpress
+#
 class profile::wordpress(
-  $db_host,
-  $root_db_password,
-  $db_password
+  String $db_host,
+  String $root_db_password,
+  String $db_password
 ) {
 
-  Class { 'profile::wordpress::database::client': before => Class['wordpress'] }
-
+  # A few extra packages that are required Wordpress to work in MySQL...plus
+  # vim
   $required_packages = [
-      'vim',
-      'wget',
-      'php-mysql'
+    'vim',
+    'wget',
+    'php-mysql'
   ]
-  
+
   package { $required_packages:
     ensure => present,
   }
 
+  # This makes sure we have the correct database clients so we are able to
+  # create databases with Puppet, client needs to match server version or
+  # things are not likely to work.
+  Class { 'profile::wordpress::database::client': before => Mysql::Db['wordpress'] }
+
+  # Handling the database creation using the defined resources type provided to
+  # us by the MySQL Puppet module (https://forge.puppet.com/puppetlabs/mysql)
+  # instead of using the support that's directly coded into the Wordpress module
+  # because its dated.
   mysql::db { 'wordpress':
     user     => 'wp-db',
     password => $db_password,
     host     => '%',
     grant    => 'ALL',
     before   => Class['wordpress'],
-  } 
+  }
+
+  # Wordpress Puppet module (https://forge.puppet.com/hunner/wordpress) that
+  # handles the retrieval, unpacking, and configuration of a Wordpress instance.
   class { 'wordpress':
     wp_owner       => 'wp-user',
     wp_group       => 'wp-group',
@@ -36,6 +58,8 @@ class profile::wordpress(
     require        => Package[$required_packages],
   }
 
+  # Wordpress module sets up a standard user but doesn't manage the home
+  # directory.
   file { '/home/wp-user':
     ensure  => directory,
     owner   => 'wp-user',
@@ -47,6 +71,9 @@ class profile::wordpress(
     owner   => 'wp-user',
     mode    => '0700',
   }
+
+  # Create our wp-user an ssh key that we'll export for later use, using a
+  # simple custom fact.
   exec { 'wp_owner_ssh_key':
     command => "ssh-keygen -N '' -f /home/wp-user/.ssh/id_rsa",
     path    => '/bin:/usr/bin',
@@ -54,14 +81,18 @@ class profile::wordpress(
     creates => '/home/wp-user/.ssh/id_rsa.pub',
     require => File['/home/wp-user/.ssh'],
   }
-  
+
+  # We're running this Wordpress instances with Apache and to manage it we'll
+  # use the tried and true Puppet module from Puppet, Inc.
+  # (https://forge.puppet.com/puppetlabs/apache)
   class { 'apache':
     default_vhost => false,
     user          => 'wp-user',
     group         => 'wp-group',
     mpm_module    => 'prefork',
   }
-  
+
+  # Enable PHP Apache module, requires us to switch to prefork
   include apache::mod::php
   include apache::mod::prefork
 
@@ -73,6 +104,9 @@ class profile::wordpress(
     block      => 'scm',
   }
 
+  # This is where Wordpress will be installed, Class['wordpress'] requires
+  # this directory implicitly so even though it is down here, it'll happen
+  # before Wordpress is installed.
   file { '/opt/wordpress':
     ensure  => directory,
     owner   => 'wp-user',
@@ -80,7 +114,9 @@ class profile::wordpress(
     mode    => '0775',
     seltype => 'httpd_var_lib_t',
   }
-  
+
+  # These directories aren't created on install but we need it to exist ahead
+  # of time so we can copy our source images into it.
   file { '/opt/wordpress/wp-content':
     ensure  => directory,
     owner   => 'wp-user',
@@ -88,7 +124,7 @@ class profile::wordpress(
     mode    => '0775',
     seltype => 'httpd_var_lib_t',
   }
-  
+
   file { '/opt/wordpress/wp-content/uploads':
     ensure  => directory,
     owner   => 'wp-user',
